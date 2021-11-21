cf5b3dd498ad69db4d3f35a2fd0ca98e583e9118
HongseockKim <gusduswk610@gmail.com>
2021-11-21 18:32:53 +0900

    e

---
+++ client/src/theme.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+exports.drakTheme = exports.lightTheme = void 0;
+exports.lightTheme = {
+    bgColor: 'white',
+    textColor: '#4b4b4b',
+    btnColor: 'tomato'
+};
+exports.drakTheme = {
+    bgColor: '#4b4b4b',
+    textColor: 'white',
+    btnColor: 'tomato'
+};
+//# sourceMappingURL=theme.js.map
